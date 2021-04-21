548f238fa258412ce500bfdd2e4ad4e2b08478c2
fatandaz <fatandaz@gmail.com>
2021-04-21 20:54:18 +0200

    PruebaBundle is ok mediante - http://localhost:8000/prueba

--- src/PruebaBundle/Controller/DefaultController.php
+++ src/PruebaBundle/Controller/DefaultController.php
@@ -8,10 +8,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/prueba", name="prueba")
      */
     public function indexAction()
     {
-        return $this->render('PruebaBundle:Default:index.html.twig');
+        return $this->render('prueba/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
     }
 }
